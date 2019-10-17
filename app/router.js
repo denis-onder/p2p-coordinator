2280935bc45245944d51430fc869286ff215cb39
Denis Onder <denis_onder@protonmail.com>
2019-10-17 13:41:54 +0200

    Disabled session routes for the time being - Focus shifting towards the peer requests, the REST API can wait for now

--- app/router.js
+++ app/router.js
@@ -1,12 +1,12 @@
 const { resolve } = require('path');
-const { createSession, getSession, endSession } = require('./controller');
+// const { createSession, getSession, endSession } = require('./controller');
 const { environment } = require('./config').server;
 
 module.exports = (app) => {
   app.get('/', (_, res) => res.send('NachoNight Peer2Peer Coordinator'));
-  app.post('/session/create', (req, res) => createSession(req, res));
-  app.get('/session/get/:id', (req, res) => getSession(req, res));
-  app.delete('/session/end/:id', (req, res) => endSession(req, res));
+  // app.post('/session/create', (req, res) => createSession(req, res));
+  // app.get('/session/get/:id', (req, res) => getSession(req, res));
+  // app.delete('/session/end/:id', (req, res) => endSession(req, res));
   // Testing client should be available while in development mode only.
   if (environment === 'development')
     app.get('/test', (_, res) =>
