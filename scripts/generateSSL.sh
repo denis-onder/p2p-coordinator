11952277804866ddb4a51ae95cd29bb07f929668
Denis Onder <denis_onder@protonmail.com>
2019-10-19 15:49:57 +0200

    Implemented a completely automatic SSL key/cert pair generator script

--- scripts/generateSSL.sh
+++ scripts/generateSSL.sh
@@ -12,6 +12,6 @@ rm *domain*
 
 echo Generating SSL Key/Cert pair...
 
-openssl req \
-  -newkey rsa:2048 -nodes -keyout domain.key \
-  -x509 -days 365 -out domain.crt
+openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 \
+    -subj "/C=US/ST=Denial/L=Denial/O=Denial/CN=www.example.com" \
+    -keyout domain.key -out domain.crt
